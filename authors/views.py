427db23fc24edbd921fb6b9fd09215a6afccfd39
Jadson-Jose <jadsonjosesilva@gmail.com>
2025-06-12 13:10:11 -0300

    feat: login

--- authors/views.py
+++ authors/views.py
@@ -1,33 +1,42 @@
-from django.shortcuts import redirect, render
-from django.http import Http404
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
+from django.shortcuts import redirect, render
 from django.urls import reverse
-
 from authors.templates.authors.forms.login import LoginForm
 from authors.templates.authors.forms.register_form import RegisterForm
+from recipes.models import Recipe
+
 
 def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
-        'form_action': reverse("authors:create"),
+        'form_action': reverse('authors:register_create'),
     })
+
 
 def register_create(request):
     if not request.POST:
         raise Http404()
+
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
-    
+
     if form.is_valid():
-        form.save()
-        messages.success(request, 'Your user is create, please login.')
-        
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Your user is created, please log in.')
+
         del(request.session['register_form_data'])
-    
+        return redirect(reverse('authors:login'))
+
     return redirect('authors:register')
+
 
 def login_view(request):
     form = LoginForm()
@@ -36,5 +45,25 @@ def login_view(request):
         'form_action': reverse('authors:login_create')
     })
 
+
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in.')
+            login(request, authenticated_user)
+        else:
+            messages.error(request, 'Invalid credentials')
+    else:
+        messages.error(request, 'Invalid username or password')
+
+    return redirect(reverse('authors:login'))
